333e914fb1746c56511ef798eee1e3ba6e8f3a6c
Paul Rivera <itspaulrivera@gmail.com>
2022-10-17 17:43:30 -0700

    Rewrite and restyle Portfolio component.

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
-import "./portfolio.css";
-import { projects } from "./data";
+import './portfolio.css';
+import { projects } from './data';
 
 const Portfolio = () => {
-    return (
-        <section id="portfolio" className="portfolio section">
-            <h2 className="section__title">Portfolio</h2>
-            <span className="section__subtitle">A few of my favorite projectes.</span>
-            <div className="portfolio__container container grid">
-
-                {projects.map((project) => (
-                    <div key={project.id} className="portfolio__content">
-                        <div className="portfolio__box">
-                            <a
-                                href={project.link}
-                                className="portfolio__link">
-                                <div className="portfolio__group">
-                                    <img
-                                        alt="gallery"
-                                        className="portfolio__img"
-                                        src={project.image}
-                                    />
-                                    <div className="portfolio__text-section">
-                                        <h1 className="portfolio__title">
-                                            {project.title}
-                                        </h1>
-                                        <h2 className="portfolio__subtitle">
+  return (
+    <section id="portfolio" className="portfolio section">
+      <h2 className="section__title">Portfolio</h2>
+      <span className="section__subtitle">A few of my favorite projectes.</span>
+      <div className="portfolio__container container grid">
+        {projects.map((project) => (
+          <div key={project.id} className="portfolio__content">
+            <div className="portfolio__box">
+              <div className="portfolio__group">
+                <img
+                  alt="gallery"
+                  className="portfolio__img"
+                  src={project.image}
+                />
+                <div className="portfolio__text-section">
+                  <h1 className="portfolio__title hover-underline-animation">
+                    <a href={project.link} className="portfolio__link">
+                      {project.title} →
+                    </a>
+                  </h1>
+                  {/* <h2 className="portfolio__subtitle">
                                             {project.subtitle}
-                                        </h2>
+                                        </h2> */}
 
-                                        <p className="portfolio__text">{project.description}</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                ))}
-
+                  <p className="portfolio__text">{project.description}</p>
+                  <div className="portfolio__tag-section">
+                    {project.tags.map((tag) => (
+                      <span className="portfolio__tags" key={tag.index}>
+                        #{tag}
+                      </span>
+                    ))}
+                  </div>
+                </div>
+              </div>
             </div>
-        </section>
-    )
-}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default Portfolio
+export default Portfolio;
